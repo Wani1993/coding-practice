ac348719162fb0bc4b1fe7a2f7eb5fb2bfd65e94
Wani1993 <104603771+Wani1993@users.noreply.github.com>
2022-06-12 21:51:00 +0900

    Add files via upload

--- Mogu/220610-03/src/Main2.java
+++ Mogu/220610-03/src/Main2.java
@@ -6,19 +6,47 @@ class Can {
 		this.name = name;
 	}
 	
+	
+
+	public Can() {
+	
+	}
+
+
+
 	public String toString() {
 		return name;
 	}
 	
-	public Can(int name) {
+	public Can Can(int name) {
 		if (name == 1) {
-			this.name = "콜라";
+			System.out.println("COLA");
+			return new Can("COLA");
 		} else if (name == 2) {
-			this.name = "스프라이트";
+			System.out.println("SPRITE");
+			return new Can("SPRITE");
 		} else if (name == 3) {
-			this.name = "환타";
+			System.out.println("FANTA");
+			return new Can("FANTA");
 		} else {
-			this.name = "핫식스";
+			System.out.println("HOTSIX");
+			return new Can("HOTSIX");
+		}
+	}
+	
+	public Can Can(String name) {
+		if (name.equals("COLA")) {
+			System.out.println("콜라");
+			return new Can("COLA");
+		} else if (name.equals("SPRITE")) {
+			System.out.println("사이다");
+			return new Can("SPRITE");
+		} else if (name.equals("FANTA")) {
+			System.out.println("환타");
+			return new Can("FANTA");
+		} else {
+			System.out.println("핫식스");
+			return new Can("HOTSIX");
 		}
 	}
 	
@@ -33,12 +61,13 @@ public class Main2 {
 	
 	// 자판기 메소드
 	// 정수형 1 -> Can 객체 : 콜라
-	
 	// 문자열 COLA -> Can 객체 : 콜라
 	
 	public static void main(String[] args) {
-		Can can = new Can(1);
+		Can can = new Can();
+		can.Can("SPRITE");
 		
-		System.out.println(can);
+		
+		
 	}
 }
