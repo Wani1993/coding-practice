d49a8723df93749fd210e6cd9a1153ddbd74c30d
Wani1993 <104603771+Wani1993@users.noreply.github.com>
2022-07-11 23:55:32 +0900

    Add files via upload

--- Mogu/220711-01data output/src/Email.java
+++ Mogu/220711-01data output/src/Email.java
@@ -1,15 +1,17 @@
 import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
-class People implements Serializable{
+class People implements Serializable {
 	private int num;
 	private String name;
 	private String phone;
@@ -66,34 +68,102 @@ public class Email {
 	public static void main(String[] args) {
 		List<People> list = new ArrayList<>();
 		ObjectOutputStream dis = null;
-				
-				
+		ObjectInputStream ois = null;
+		Scanner scan = new Scanner(System.in);
+		list.add(new People(1, "홍길동", "011-111-1111", "hong@hanmail.net"));
+		list.add(new People(2, "김유신", "010-222-2222", "wan@hanmail.net"));
+		boolean end = true;
+		
+
+		while (end) {
+			System.out.println(list);
+			System.out.println("목록을 선택하세요.");
+			System.out.println("1번 사람등록	2번 모든 목록");
+			System.out.println("3번 등록삭제	4번 프로그램종료");
+			int num = scan.nextInt();
+			switch (num) {
+
+			case 1:
+				System.out.println("순서, 이름, 전화번호, 이메일을 입력하세요");
+
+				int a = scan.nextInt();
+				if (a <= list.size()) {
+					System.out.println("이미 등록된 번호입니다.");
+				} else {
+					Scanner sc = new Scanner(System.in);
+					String name = sc.nextLine();
+					String phone = sc.nextLine();
+					String Email = sc.nextLine();
+					list.add(new People(a, name, phone, Email));
+				}
+				break;
+			case 2:
+				System.out.println("1.모든목록    2.순서별 정보");
+				int menu = scan.nextInt();
+				if(menu == 1) {
+				System.out.println(list);
+				} else if (menu == 2) {
+					System.out.println("몇번의 정보를 볼건가유?");
+					int what = scan.nextInt();
+					System.out.println(list.get(what - 1));
+				}
+				break;
+
+			case 3:
+				System.out.println("몇번째 회원을 삭제하실건가유?");
+				int b = scan.nextInt();
+				list.remove(b);
+				break;
+
+			case 4:
+				System.out.println("시스템을 종료합니다.");
+				end = false;
+				break;
+			default:
+				System.out.println("잘못입력하였습니다.");
+				break;
+			}
+			
+			
+		}
+
+		
+
 		try {
+
 			dis = new ObjectOutputStream(new FileOutputStream(new File("d:\\filetest\\People.dat")));
-			
-				dis.writeObject(new People(1, "홍길동", "011-111-1111", "hong@hanmail.net"));
-				dis.writeObject(new People(2, "김유신", "010-222-2222", "hong@hanmail.net"));
-			
+
+			for (int i = 0; i < list.size(); i++) {
+				dis.writeObject(list.get(i));
+			}
 			dis.flush();
-			
-			
+
+			ois = new ObjectInputStream(new FileInputStream(new File("d:\\filetest\\People.dat")));
+			for (int i = 0; i < list.size(); i++) {
+				People p = (People) ois.readObject();
+				System.out.println(p);
+			}
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (EOFException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}  finally {
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
 			try {
-				if(dis != null) {
+				if (dis != null) {
 					dis.close();
+				}
+				if (ois != null) {
+					ois.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		
-		
+
 	}
 }
