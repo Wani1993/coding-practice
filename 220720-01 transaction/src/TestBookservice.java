3ffd721579e9c41ef0b7b2456cdd052c09555504
Wani1993 <104603771+Wani1993@users.noreply.github.com>
2022-07-20 17:52:23 +0900

    Delete 220720-01 transaction directory

--- 220720-01 transaction/src/TestBookservice.java
+++
@@ -1,7 +0,0 @@
-public class TestBookservice {
-	public static void main(String[] args) {
-		BookService service = new BookService(new BookDaoParamConn());
-		service.insertAndUpdate("중복", 1000);
-//		service.insertAndUpdate("중복이름", 660);
-	}
-}
